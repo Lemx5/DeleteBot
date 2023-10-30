28157e768b6dded8a27c74c82d763e2b60849f9a
ｒｙｍｅ <ryme.care@hotmail.com>
2023-10-31 01:23:30 +0530

    updated

--- bot.py
+++ bot.py
@@ -45,25 +45,17 @@ async def clear_chat(client, message):
         await message.reply("You must be an admin to use this command!")
         return
     
-    # Store message IDs in a dictionary
-    message_ids = {}
+    # Delete messages using the direct delete method
     async for msg in app.iter_history(chat_id):
-        message_ids[msg.message_id] = True
-
-    # Delete messages using the stored IDs
-    index = 0
-    msg_ids_list = list(message_ids.keys())
-
-    while index < len(msg_ids_list):
         try:
-            await app.delete_messages(chat_id, msg_ids_list[index])
-            print(f"Deleted message {msg_ids_list[index]}")
-            index += 1  # Move to the next message only after successful deletion
+            await msg.delete()
+            print(f"Deleted message {msg.message_id}")
         except FloodWait as e:
             print(f"Rate limit exceeded. Sleeping for {e.x} seconds.")
             await asyncio.sleep(e.x)
 
-    await message.reply(f"Deleted {len(message_ids)} messages from {message.chat.title}!")
+    await message.reply(f"Deleted messages from {message.chat.title}!")
+
 
 def run():
     app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8080)))
